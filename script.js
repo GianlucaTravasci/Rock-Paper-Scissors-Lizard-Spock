25f8c0b7d02d7d00e78c8b1de8418aef436c73ef
GianlucaTravasci <gianlucatravasci@gmail.com>
2020-08-26 17:38:31 +0200

    add computer random choice, display of the computer choice and the function to check how win the round

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+//Game status
+const playerScoreElement = document.getElementById('playerScore');
+const playerChoiceElemente = document.getElementById('playerChoice');
+const computerScoreElement = document.getElementById('computerScore');
+const computerChoiceElement = document.getElementById('computerChoice');
+const resultText = document.getElementById('resultText')
 
+//User game choice
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissor = document.getElementById('playerScissor');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+//Computer game choice
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissor = document.getElementById('computerScissor');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -7,4 +26,71 @@ const choices = {
   lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
+
+let computerChoice = '';
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+
+
+// Computer choice his moves 
+function computerRandomChoice() {
+  const random = Math.random();
+  if(random <= 0.2) {
+    computerChoice = 'rock';
+  } else if (random <= 0.4) {
+    computerChoice = 'paper';
+  } else if (random <= 0.6) {
+    computerChoice = 'scissors';
+  } else if (random <= 0.8) {
+    computerChoice = 'lizard';
+  } else {
+    computerChoice = 'spock';
+  }
+}
+
+//Add the class selected to the img of the computer's chioce in order to paint it black 
+function displayComputerPick() {
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceElement.textContent = '--- Rock'
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceElement.textContent = '--- Paper'
+      break;
+    case 'scissors':
+      computerScissor.classList.add('selected');
+      computerChoiceElement.textContent = '--- Scissors'
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceElement.textContent = '--- Lizzard'
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceElement.textContent = '--- Spock'
+      break;
+    default:
+      break;
+  }
+}
+
+function checkWinner(playerChoice) {
+  if(playerChoice === computerChoice) {
+    resultText.textContent = 'Tie!!';
+  } else {
+    const choice = choices[playerChoice];
+    if(choice.defeats.indexOf(computerChoice) < -1) {
+      playerScoreNumber++;
+      resultText.textContent = 'You Won!';
+      playerScoreElement.textContent = playerScoreNumber;
+    } else {
+      computerScoreNumber++;
+      resultText.textContent = 'You Lost!';
+      computerScoreElement = computerScoreNumberM
+    }
+  }
+}
+
 
